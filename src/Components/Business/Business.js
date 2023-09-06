9b93cdc830f6cb6e4754ef8aed912b13bfa20cea
Dody <kristiglasow@kristis-mbp.lan>
2023-09-06 13:24:31 -0600

    Added components, and foler / file structure.

---
+++ src/Components/Business/Business.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Business = {
+    imageSrc: '/Users/kristiglasow/Documents/Codys Web dev/Images/skateboardshop.jpeg',
+    name: 'Dodes Skate Shop',
+    address: '43 Skaters Cape SE',
+    city: 'Calgary',
+    province: 'Alberta',
+    postalcode: 'T3A 4B5',
+    category: 'skateboard',
+    rating: '4.5',
+    reviewcount: '90'
+
+};
+
+
+export default Business;
